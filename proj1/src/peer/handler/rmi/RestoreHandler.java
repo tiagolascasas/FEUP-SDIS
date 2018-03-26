a1513828d0d0638ffd2e7da313558fdd6eaffc54
tiagolascasas <up201503616@fe.up.pt>
2018-03-26 19:01:53 +0100

    RestoreHandler

--- proj1/src/peer/handler/rmi/RestoreHandler.java
+++ proj1/src/peer/handler/rmi/RestoreHandler.java
@@ -1,19 +1,27 @@
 package peer.handler.rmi;
 
+import peer.CHANNELS;
 import peer.handler.Handler;
+import peer.message.MessageGetchunk;
 
 public class RestoreHandler extends Handler
 {
-	public RestoreHandler(String fileId)
+	private String fileId;
+	private int numberOfChunks;
+
+	public RestoreHandler(String fileId, int numberOfChunks)
 	{
-		
+		this.fileId = fileId;
+		this.numberOfChunks = numberOfChunks;
 	}
 	
 	@Override
 	public void run()
 	{
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < this.numberOfChunks; i++)
+		{
+			MessageGetchunk message = new MessageGetchunk(fileId.getBytes(), i);
+			send(CHANNELS.MC, message.getMessageBytes());
+		}
 	}
-
 }
