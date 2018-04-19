6aac8b8df07b5db73f1bc619f3dfc2e5b4bb4fd5
tiagolascasas <up201503616@fe.up.pt>
2018-04-19 15:51:55 +0100

    proj2

---
+++ proj2/src/client/Server.java
@@ -0,0 +1,23 @@
+package client;
+
+import server.Client;
+
+public class Server
+{
+	public Server()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO parse args
+		Server server = new Server();
+		server.run();
+	}
+
+	private void run()
+	{
+		
+	}
+}
